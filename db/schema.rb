9d8c5986950a3d06c3f55f3ca36f7758264c1c4e
Linda Barba <lindabarba13@gmail.com>
2017-04-25 06:23:24 -0700

    add image attribute to comment tacospot

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425054952) do
+ActiveRecord::Schema.define(version: 20170425132108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170425054952) do
     t.string   "content"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "image_url"
   end
 
   create_table "tacospots", force: :cascade do |t|
@@ -31,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170425054952) do
     t.boolean  "cash_only"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "image_url"
   end
 
   create_table "users", force: :cascade do |t|
