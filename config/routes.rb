2ff47952338ee54d17620df5c297eca84ae0b732
Linda Barba <lindabarba13@gmail.com>
2017-04-24 20:22:05 -0700

    add user authentication persistent login sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
 end
