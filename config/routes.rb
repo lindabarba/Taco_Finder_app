5d9402dbf3135e8fcadeddb48352d986ff0e511c
Linda Barba <lindabarba13@gmail.com>
2017-04-25 09:06:32 -0700

    add user tacospot reference

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users
+  resources :tacospots
+  resources :comments
   resources :sessions, only: [:new, :create, :destroy]
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
